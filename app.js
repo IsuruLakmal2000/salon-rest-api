9efff32511a8c980f5e9055cb0f23958aca36a2b
isuru lakmal <isurulakmal200013@gmail.com>
2023-05-24 16:59:50 +0530

    app port assigned dynamically

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRouter = require('./api/users/user_router');
-
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use('/api/users', userRouter);
 
 
-app.listen(process.env.APP_PORT, () => {
+app.listen(port, () => {
     console.log('Server up and running on PORT: ', process.env.APP_PORT);
 });
 
