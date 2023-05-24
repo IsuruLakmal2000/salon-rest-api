636c32f507833bba90314adfb087eb20ed5bd632
isuru lakmal <isurulakmal200013@gmail.com>
2023-05-24 15:47:21 +0530

    dasda.

--- app.js
+++ app.js
@@ -11,3 +11,6 @@ app.use('/api/users', userRouter);
 app.listen(process.env.APP_PORT, () => {
     console.log('Server up and running on PORT: ', process.env.APP_PORT);
 });
+
+module.exports = app;
+//https://blade-salon-app.herokuapp.com/ | https://git.heroku.com/blade-salon-app.git
