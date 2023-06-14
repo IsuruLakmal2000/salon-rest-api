244203714bd670bc58e880f66974e9fcc9dd7c91
isuru lakmal <isurulakmal200013@gmail.com>
2023-06-14 12:27:17 +0530

    .

--- api/salons/salon_service.js
+++ api/salons/salon_service.js
@@ -60,7 +60,7 @@ module.exports = {
                 if(error){
                     return callBack(error);
                 }
-                return callBack(null,results);
+                return callBack(null,results[0]);
             }
         );
     }
