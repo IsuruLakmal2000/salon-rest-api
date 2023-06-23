5d72e78f4176bcd5849f9a2d5dcf4cde8257a359
isuru lakmal <isurulakmal200013@gmail.com>
2023-06-24 04:17:03 +0530

    .

--- api/feedbacks/feedback_router.js
+++ api/feedbacks/feedback_router.js
@@ -2,7 +2,7 @@ const{getFeedback} = require('./feedback_controller');
 const router = require('express').Router();
 
  
- router.get('/', getFeedback); 
+ router.get('/:salon_id', getFeedback); 
 // router.post('/salonType',addSalonType); 
 
 module.exports = router;
