fd5b7ff9b145eb99ebdd5af233b8a4c1ca7a955b
isuru lakmal <isurulakmal200013@gmail.com>
2023-06-24 04:40:26 +0530

    .

--- api/feedbacks/feedback_service.js
+++ api/feedbacks/feedback_service.js
@@ -10,7 +10,7 @@ module.exports = {
                 if(error){
                     return callBack(error);
                 }
-                return callBack(null,results);
+                return callBack(null,results[0]);
             }
         );
     }
