7087ccc67c8c0f1ab9a5d2c53662818396ab3b8a
isuru lakmal <isurulakmal200013@gmail.com>
2023-06-24 04:55:10 +0530

    .

--- api/feedbacks/feedback_controller.js
+++ api/feedbacks/feedback_controller.js
@@ -6,7 +6,7 @@ module.exports = {
     
     getFeedback: (req, res) => {
         console.log(req.params.salon_id);
-        const {salonId} = req.params.salon_id;
+        const salonId = req.params.salon_id;
         console.log(salonId);
         getFeedbackBySalonId(salonId,(err,results) => {
             if(err){
