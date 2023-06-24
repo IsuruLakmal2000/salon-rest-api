faf3ad99ad1e1f3d5e2458bf2a6652e13943a515
isuru lakmal <isurulakmal200013@gmail.com>
2023-06-24 13:23:42 +0530

    .

--- api/feedbacks/feedback_service.js
+++ api/feedbacks/feedback_service.js
@@ -5,7 +5,7 @@ module.exports = {
         pool.query(
            //'select * from feedback where salon_id=?',
 
-           'SELECT feedback.feedback_id, feedback.feedback,feedback.rating_star, customer.customer_name FROM feedback JOIN customer ON feedback.customer_id = customer.customer_id WHERE feedback.salon_id = ?'
+           'SELECT feedback.feedback_id, feedback.feedback,feedback.rating_star, customer.customer_name FROM feedback JOIN customer ON feedback.customer_id = customer.customer_id WHERE feedback.salon_id = ?',
           
             [salonId],
             (error,results,fields)=>{
