c1a3124c359eb3512f1ad27cff3f525929b3dcf2
isuru lakmal <isurulakmal200013@gmail.com>
2023-06-24 13:14:23 +0530

    .

--- api/feedbacks/feedback_service.js
+++ api/feedbacks/feedback_service.js
@@ -3,7 +3,9 @@ const pool = require('../../config/database');
 module.exports = {
     getFeedbackBySalonId : (salonId,callBack)=>{
         pool.query(
-           'select * from feedback where salon_id=?',
+           //'select * from feedback where salon_id=?',
+
+           'SELECT feedback.feedback_id, feedback.feedback,feedback.rating_star, customer.customer_name FROM feedback JOIN customer ON feedback.customer_id = customer.customer_id WHERE feedback.salon_id = ?'
            //'select * from feedback',
             [salonId],
             (error,results,fields)=>{
