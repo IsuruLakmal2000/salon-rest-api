28c69917d276e7502a3092e09d1a66dda31914ab
isuru lakmal <isurulakmal200013@gmail.com>
2023-07-20 01:39:38 +0530

    ..

--- api/salons/salon_controller.js
+++ api/salons/salon_controller.js
@@ -90,7 +90,7 @@ module.exports = {
                     success: 1,
                     message: "login successsfully",
                     token: jsontoken,
-                    result: results
+                    result: result
                 });
             }else{
                 console.log(result);
