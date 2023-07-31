68a57aa11fef189198feb5c9173ec7057b394fe9
isuru lakmal <isurulakmal200013@gmail.com>
2023-08-01 01:43:43 +0530

    ..

--- api/customers/customer_service.js
+++ api/customers/customer_service.js
@@ -1,65 +1,66 @@
-const pool = require('../../config/database'); 
+const pool = require("../../config/database");
 
 module.exports = {
-    create : (data, callBack) => {
-        pool.query(
-            'insert into customer(customer_name,email,password) values(?,?,?)',
-            [
-                data.customer_name,
-                data.email,
-                data.password,
-               
-            ],
-            (error, results, fields) => {
-                if(error){
-                    return callBack(error);
-                }
-                return callBack(null, results);
-            }
-        )
-    },
-   
-    getCustomer: callback =>{
-        pool.query(
-            'select * from customer',
-            [   ],
-            (error, results, fields) => {
-                if(error){
-                    return callback(error);
-                }
-                return callback(null, results);
-            }
-        );
+  create: (data, callBack) => {
+    pool.query(
+      "insert into customer(customer_name,email,password) values(?,?,?)",
+      [data.customer_name, data.email, data.password],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
 
-    },
-    // updateCustomer: (data, callback) => {
-    //     pool.query(
-    //         'update customer set description=? where salon_id=?',
-    //         [
-    //             data.description,
-              
-    //             data.id
-    //         ],
-    //         (error, results, fields) => {
-    //             if(error){
-    //                 return callback(error);
-    //             }
-    //             return callback(null, results);
-    //         }
-    //     );
-    // },
+  getCustomer: (callback) => {
+    pool.query("select * from customer", [], (error, results, fields) => {
+      if (error) {
+        return callback(error);
+      }
+      return callback(null, results);
+    });
+  },
+  getCustomerById: (customer_id, callback) => {
+    pool.query(
+      "select * from customer where customer_id=?",
+      [customer_id],
+      (error, results, fields) => {
+        if (error) {
+          return callback(error);
+        }
+        return callback(null, results);
+      }
+    );
+  },
+  // updateCustomer: (data, callback) => {
+  //     pool.query(
+  //         'update customer set description=? where salon_id=?',
+  //         [
+  //             data.description,
 
-    getUserByEmail : (email,callBack)=>{
-        pool.query(
-            'select * from customer where email=?',
-            [email],
-            (error,results,fields)=>{
-                if(error){
-                    return callBack(error);
-                }
-                return callBack(null,results[0]);
-            }
-        );
-    }
+  //             data.id
+  //         ],
+  //         (error, results, fields) => {
+  //             if(error){
+  //                 return callback(error);
+  //             }
+  //             return callback(null, results);
+  //         }
+  //     );
+  // },
 
-}
+  getUserByEmail: (email, callBack) => {
+    pool.query(
+      "select * from customer where email=?",
+      [email],
+      (error, results, fields) => {
+        if (error) {
+          return callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+};
