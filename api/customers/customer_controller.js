3d37af9083b4de17f705c0a3a1d06628db8f39d0
isuru lakmal <isurulakmal200013@gmail.com>
2023-08-01 01:33:18 +0530

    ..

--- api/customers/customer_controller.js
+++ api/customers/customer_controller.js
@@ -1,99 +1,114 @@
-
-const {create, getCustomer,updateCustomer,getUserByEmail} = require('./customer_service');
-const { genSaltSync, hashSync ,compareSync } = require('bcrypt');
-const { sign } = require('jsonwebtoken');
-
+const {
+  create,
+  getCustomer,
+  updateCustomer,
+  getUserByEmail,
+  getCustomerById,
+} = require("./customer_service");
+const { genSaltSync, hashSync, compareSync } = require("bcrypt");
+const { sign } = require("jsonwebtoken");
 
 module.exports = {
-    createCustomer: (req, res) => {
-        const body = req.body;
-        console.log(req.body);
-        //-----------salon password encryption----------
-        const salt = genSaltSync(10);
-        body.password = hashSync(body.password, salt);
+  createCustomer: (req, res) => {
+    const body = req.body;
+    console.log(req.body);
+    //-----------salon password encryption----------
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
 
-        //-----------
-        create(body,(err, results) => {
-            if(err){
-                console.log(err);
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection error"
-                });
-            }
-            return res.status(200).json({
-                success: 1,
-                data: results
-            });
+    //-----------
+    create(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection error",
         });
-    },
-    
-    updateCustomer: (req, res) => {
-        const body = req.body;
-        updateCustomer(body,(err,results) => {
-            if(err){
-                console.log(err);
-                return;
-            }
-            if(!results){
-                return res.json({
-                    success: 0,
-                    message: "Failed to update user"
-                });
-            }
-            return res.json({
-                success: 1,
-                message: "updated successfully"
-            });
-        });
-    },
-    getCustomer: (req, res) => {
-        getCustomer((err,results) => {
-            if(err){
-                console.log(err);
-                return;
-            }
-            return res.json({
-                success: 1,
-                data: results
-            });
-        });
-    },
+      }
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
 
-    login: (req, res) => {
-        const {email,password} = req.body;
-        getUserByEmail(email, (err, results) => {
-            if(err){
-                console.log(err);
-            }
-            if(!results){
-                return res.json({
-                    success: 0,
-                    message: "Email doest not exists.Please try again "
-                });
-            }
-            const result = compareSync(password, results.password);
-            if(result){
-                results.password = undefined;
-                const jsontoken = sign({result: results}, process.env.JWT_KEY, {
-                    expiresIn: "1h"
-                });
-                return res.json({
-                    success: 1,
-                    message: "login successfully",
-                    token: jsontoken,
-                    result: results
-                });
-            }else{
-                console.log(result);
-                console.log(password +"-"+ results.password);
-                return res.json({
-                    
-                    success: 0,
-                    message: "Invalid email or password.please try again"
-                });
-            }
+  updateCustomer: (req, res) => {
+    const body = req.body;
+    updateCustomer(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (!results) {
+        return res.json({
+          success: 0,
+          message: "Failed to update user",
         });
-    },
-    
+      }
+      return res.json({
+        success: 1,
+        message: "updated successfully",
+      });
+    });
+  },
+  getCustomer: (req, res) => {
+    getCustomer((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      return res.json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  getCustomerById: (req, res) => {
+    const customer_id = req.params.customer_id;
+    getCustomerById(customer_id, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      return res.json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+
+  login: (req, res) => {
+    const { email, password } = req.body;
+    getUserByEmail(email, (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      if (!results) {
+        return res.json({
+          success: 0,
+          message: "Email doest not exists.Please try again ",
+        });
+      }
+      const result = compareSync(password, results.password);
+      if (result) {
+        results.password = undefined;
+        const jsontoken = sign({ result: results }, process.env.JWT_KEY, {
+          expiresIn: "1h",
+        });
+        return res.json({
+          success: 1,
+          message: "login successfully",
+          token: jsontoken,
+          result: results,
+        });
+      } else {
+        console.log(result);
+        console.log(password + "-" + results.password);
+        return res.json({
+          success: 0,
+          message: "Invalid email or password.please try again",
+        });
+      }
+    });
+  },
 };
--- api/customers/customer_router.js
+++ api/customers/customer_router.js
@@ -1,10 +1,16 @@
-const{ createCustomer, getCustomer, updateCustomer,login} = require('./customer_controller');
-const router = require('express').Router();
+const {
+  createCustomer,
+  getCustomer,
+  updateCustomer,
+  login,
+  getCustomerById,
+} = require("./customer_controller");
+const router = require("express").Router();
 
-router.post('/', createCustomer);
-router.get('/', getCustomer);
-router.patch('/', updateCustomer); 
-router.post('/login',login)
-
+router.post("/", createCustomer);
+router.get("/", getCustomer);
+router.get("/:customer_id", getCustomerById);
+router.patch("/", updateCustomer);
+router.post("/login", login);
 
 module.exports = router;
